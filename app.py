d7b20cca1aaae4814ed9aa71a268fb939c807554
sebrai <sebbranden@gmail.com>
2026-05-29 13:48:55 +0200

    del 5

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, abort, Response
+import mysql.connector
 app = Flask(__name__)
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="10.200.14.13",
+        user='lab',
+        password='lab',
+        database="lab"
+    )
 
 @app.route("/")
 def home():
@@ -12,8 +21,14 @@ def content(content):
 @app.route("/form",methods=["GET","POST"])
 def form():
     if request.method == "POST":
-        tekst = request.form["tekst"]
-        print(tekst)
+        name = request.form['name']
+        password = request.form['password']
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users(name, passord) VALUES (%s,%s)",(name,password))
+        conn.commit()
+        cursor.close()
+        conn.close()
     return redirect(url_for("home"))
 
 @app.route("/title/<tekst>")
@@ -22,7 +37,12 @@ def title(tekst):
 
 @app.route("/table")
 def table():
-    table = [[1,3,2],[5,5,7],[1,10,0]]
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name, passord FROM users")
+    table = cursor.fetchall()
+    cursor.close()
+    conn.close()
     return render_template("table.html",table = table)
 if __name__ == "__main__":
 
